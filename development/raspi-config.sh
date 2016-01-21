10aaad9103e096ca3f61f40a16614c922bb24d6e
Matthew Hsu <gears88@gmail.com>
2016-01-21 12:01:57 -0500

    Add missing config functions

--- development/raspi-config.sh
+++ development/raspi-config.sh
@@ -9,6 +9,42 @@ then
     echo "The script needs to run as root" && exit 1
 fi
 
+set_config_var() {
+  lua - "$1" "$2" "$3" <<EOF > "$3.bak"
+  local key=assert(arg[1])
+  local value=assert(arg[2])
+  local fn=assert(arg[3])
+  local file=assert(io.open(fn))
+  local made_change=false
+  for line in file:lines() do
+    if line:match("^#?%s*"..key.."=.*$") then
+      line=key.."="..value
+      made_change=true
+    end
+    print(line)
+  end
+  if not made_change then
+    print(key.."="..value)
+  end
+  EOF
+  mv "$3.bak" "$3"
+}
+
+get_config_var() {
+  lua - "$1" "$2" <<EOF
+  local key=assert(arg[1])
+  local fn=assert(arg[2])
+  local file=assert(io.open(fn))
+  for line in file:lines() do
+    local val = line:match("^#?%s*"..key.."=(.*)$")
+    if (val ~= nil) then
+      print(val)
+    break
+  end
+end
+EOF
+}
+
 # Create blacklist file
 if ! [ -e $BLACKLIST ]; then
     touch $BLACKLIST
