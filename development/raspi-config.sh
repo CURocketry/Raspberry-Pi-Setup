98689485090545d1d8f0ae60968650ffeab36a3f
Matthew Hsu <gears88@gmail.com>
2016-01-21 11:52:41 -0500

    Add raspberry pi hardware configuration

---
+++ development/raspi-config.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+CONFIG=/boot/config.txt
+BLACKLIST=/etc/modprobe.d/raspi-blacklist.conf
+
+# Exit if the script was not launched by root or through sudo
+if [[ $UID != 0 ]];
+then
+    echo "The script needs to run as root" && exit 1
+fi
+
+# Create blacklist file
+if ! [ -e $BLACKLIST ]; then
+    touch $BLACKLIST
+fi
+
+# disable device tree
+#sed $CONFIG -i -e "s/^\(device_tree=\)$/#\1/"
+#sed $CONFIG -i -e "s/^#\(device_tree=.\)/\1/"
+
+# enable device tree
+sed $CONFIG -i -e "s/^#\(device_tree=\)$/\1/"
+sed $CONFIG -i -e "s/^\(device_tree=.\)/#\1/"
+if ! grep -q "^device_tree=$" $CONFIG; then
+    printf "device_tree=\n" >> $CONFIG
+fi
+
+# enable i2c
+SETTING=on
+sed $CONFIG -i -r -e "s/^((device_tree_param|dtparam)=([^,]*,)*i2c(_arm)?)(=[^,]*)?/\1=$SETTING/"
+if ! grep -q -E "^(device_tree_param|dtparam)=([^,]*,)*i2c(_arm)?=[^,]*" $CONFIG; then
+    printf "dtparam=i2c_arm=$SETTING\n" >> $CONFIG
+fi
+
+# load the I2C lernal module by default
+sed $BLACKLIST -i -e "s/^\(blacklist[[:space:]]*i2c[-_]bcm2708\)/#\1/"
+modprobe i2c-bcm2708
+
+# Enable the raspberry pi camera
+set_camera() {
+    # Stop if /boot is not a mountpoint
+    if ! mountpoint -q /boot; then
+        return 1
+        fi
+
+    [ -e $CONFIG ] || touch $CONFIG
+
+    if [ "$1" -eq 0 ]; then # disable camera
+        set_config_var start_x 0 $CONFIG
+        sed $CONFIG -i -e "s/^startx/#startx/"
+        sed $CONFIG -i -e "s/^start_file/#start_file/"
+        sed $CONFIG -i -e "s/^fixup_file/#fixup_file/"
+    else # enable camera
+        set_config_var start_x 1 $CONFIG
+        CUR_GPU_MEM=$(get_config_var gpu_mem $CONFIG)
+        if [ -z "$CUR_GPU_MEM" ] || [ "$CUR_GPU_MEM" -lt 128 ]; then
+            set_config_var gpu_mem 128 $CONFIG
+        fi
+        sed $CONFIG -i -e "s/^startx/#startx/"
+        sed $CONFIG -i -e "s/^fixup_file/#fixup_file/"
+    fi
+}
+set_camera 1
--- development/setup.sh
+++ development/setup.sh
@@ -1,4 +1,52 @@
 #!/bin/bash
 
-sudo ./root_tasks.sh
-./cuser_tasks.sh
+sudo ./raspi-config.sh
+
+# Before changing directories:
+#
+# Copy the udev ttyusb aliases
+sudo cp ../99-usb-serial.rules /etc/udev/rules.d
+
+# Install required programs
+sudo apt-get -q -qq update
+# Breaks on Ubuntu 14.04 VM
+#apt-get -q -y dist-upgrade
+sudo apt-get -y install git vim build-essential g++ tmux
+sudo apt-get -y install gpsd libgps-dev gpsd-clients python-gps libi2c-dev
+
+#give user permission to USB devices
+sudo usermod -a -G dialout $USER
+
+# TODO Use CRT forks
+
+mkddir -p ~/Desktop/lib
+# Install libxbee
+cd ~/Desktop/lib
+git clone https://github.com/attie/libxbee3
+cd libxbee3
+make configure
+make all
+sudo make install
+
+# Install wiringpi
+cd ~/Desktop/lib
+git clone git://git.drogon.net/wiringPi
+cd wiringPi
+./build
+# Enable I2C
+gpio load i2c
+
+# Copy a good vimrc
+cd ~
+git clone https://github.com/mahsu/vimrc
+cd vimrc
+chmod +x setup.sh
+./setup.sh
+
+# Clone development repos
+mkdir -p ~/Desktop/dev
+cd ~/Desktop/dev
+git clone https://github.com/curocketry/launchvehiclecontroller2016
+git clone https://github.com/mahsu/raspberrypi-adafruit10dof
+
+echo "Reboot the machine to complete setup"
