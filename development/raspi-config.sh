70e5a451fd6c6c4ac88558761fe2c3208c341681
Matthew Hsu <gears88@gmail.com>
2016-01-21 17:24:36 +0000

    Fix config functions

--- development/raspi-config.sh
+++ development/raspi-config.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Commands are sourced from https://github.com/asb/raspi-config
+
 CONFIG=/boot/config.txt
 BLACKLIST=/etc/modprobe.d/raspi-blacklist.conf
 
@@ -11,34 +13,35 @@ fi
 
 set_config_var() {
   lua - "$1" "$2" "$3" <<EOF > "$3.bak"
-  local key=assert(arg[1])
-  local value=assert(arg[2])
-  local fn=assert(arg[3])
-  local file=assert(io.open(fn))
-  local made_change=false
-  for line in file:lines() do
-    if line:match("^#?%s*"..key.."=.*$") then
-      line=key.."="..value
-      made_change=true
-    end
-    print(line)
+local key=assert(arg[1])
+local value=assert(arg[2])
+local fn=assert(arg[3])
+local file=assert(io.open(fn))
+local made_change=false
+for line in file:lines() do
+  if line:match("^#?%s*"..key.."=.*$") then
+    line=key.."="..value
+    made_change=true
   end
-  if not made_change then
-    print(key.."="..value)
-  end
-  EOF
-  mv "$3.bak" "$3"
+  print(line)
+end
+
+if not made_change then
+  print(key.."="..value)
+end
+EOF
+mv "$3.bak" "$3"
 }
 
 get_config_var() {
   lua - "$1" "$2" <<EOF
-  local key=assert(arg[1])
-  local fn=assert(arg[2])
-  local file=assert(io.open(fn))
-  for line in file:lines() do
-    local val = line:match("^#?%s*"..key.."=(.*)$")
-    if (val ~= nil) then
-      print(val)
+local key=assert(arg[1])
+local fn=assert(arg[2])
+local file=assert(io.open(fn))
+for line in file:lines() do
+  local val = line:match("^#?%s*"..key.."=(.*)$")
+  if (val ~= nil) then
+    print(val)
     break
   end
 end
