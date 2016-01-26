925ed8a73cae280290c94e7c7e63ee69274f18c8
Matthew Hsu <gears88@gmail.com>
2016-01-26 12:28:40 -0500

    Update setup, begin production setup

--- development/setup.sh
+++ development/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo ./raspi-config.sh
+sudo ../raspi-config.sh
 
 # Before changing directories:
 #
@@ -9,9 +9,9 @@ sudo cp ../99-usb-serial.rules /etc/udev/rules.d
 
 # Install required programs
 sudo apt-get -q -qq update
-# Breaks on Ubuntu 14.04 VM
-#apt-get -q -y dist-upgrade
-sudo apt-get -y install git vim build-essential g++ tmux
+#sudo apt-get -q -y dist-upgrade
+#sudo rpi-update
+sudo apt-get -y install git vim build-essential g++ python python-dev
 sudo apt-get -y install gpsd libgps-dev gpsd-clients python-gps libi2c-dev
 
 #give user permission to USB devices
@@ -36,17 +36,28 @@ cd wiringPi
 # Enable I2C
 gpio load i2c
 
+# Install zlog
+cd ~/Desktop/lib
+git clone github.com/HardySimpson/zlog
+cd zlog
+make
+sudo make install
+
 # Copy a good vimrc
 cd ~
 git clone https://github.com/mahsu/vimrc
 cd vimrc
-chmod +x setup.sh
+sudo chmod +x setup.sh
 ./setup.sh
 
 # Clone development repos
 mkdir -p ~/Desktop/dev
 cd ~/Desktop/dev
 git clone https://github.com/curocketry/launchvehiclecontroller2016
-git clone https://github.com/mahsu/raspberrypi-adafruit10dof
+
+git clone https://github.com/mahsu/RaspberryPi-Adafruit10DOF
+cd RaspberryPi-Adafruit10DOF
+make all
+sudo make install
 
 echo "Reboot the machine to complete setup"
